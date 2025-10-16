c266ca898764ca3eeeace11cbe6c585fdbcc3475
kriptonhaz <kriptonhaz@gmail.com>
2025-10-16 14:12:41 +0700

    feat: add Lottie animation support
    - Add lottie-react-native dependency
    - Update metro config to handle .lottie files

--- metro.config.js
+++ metro.config.js
@@ -3,4 +3,7 @@ const { withNativeWind } = require('nativewind/metro');
 
 const config = getDefaultConfig(__dirname);
 
+// Add support for .lottie files
+config.resolver.assetExts.push('lottie');
+
 module.exports = withNativeWind(config, { input: './global.css' });
