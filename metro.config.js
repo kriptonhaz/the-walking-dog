86f73b0cfd333f5775071aab2a870928369a500f
kriptonhaz <kriptonhaz@gmail.com>
2025-10-27 11:23:38 +0700

    refactor: remove nativewind integration and cleanup tsconfig
    Remove nativewind configuration from metro.config.js and clean up tsconfig.json by removing unused type declaration file. This simplifies the build configuration as nativewind is no longer needed.

--- metro.config.js
+++ metro.config.js
@@ -1,9 +1,8 @@
 const { getDefaultConfig } = require('expo/metro-config');
-const { withNativeWind } = require('nativewind/metro');
 
 const config = getDefaultConfig(__dirname);
 
 // Add support for .lottie files
 config.resolver.assetExts.push('lottie');
 
-module.exports = withNativeWind(config, { input: './global.css' });
+module.exports = config;
