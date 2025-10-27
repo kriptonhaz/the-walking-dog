a87c85477b285b91dd9381ca1e42aa18b174fe4e
kriptonhaz <kriptonhaz@gmail.com>
2025-10-27 13:12:09 +0700

    refactor: remove redundant global.css import in _layout.tsx

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -7,7 +7,6 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { Stack } from "expo-router";
 import { StatusBar } from "expo-status-bar";
 import "react-native-reanimated";
-import "../global.css";
 
 import { useColorScheme } from "@/hooks/use-color-scheme";
 
